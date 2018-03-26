d039d500a0df4714f8bc2de72a04c8619bdecde3
Michelle Blevins <mjblevins7@gmail.com>
2018-03-26 21:20:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,10 +71,12 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   all_holidays = []
-  "#{holiday_hash.values.flatten} BBQ"
+
   holiday_hash.each do |item, value|
     all holidays << item
   end
+
   all_holidays << "BBQ"
+
   all_holidays
 end
