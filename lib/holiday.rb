528532e4a0d89f1b5be26e2c2500cf77c2743fc2
Michelle Blevins <mjblevins7@gmail.com>
2018-03-26 21:06:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |item, value|
-    puts "#{item}:"
+    puts "#{item.capitalize}:"
    item.each do |value, data|
      puts "#{value}: #{data.join(", ")}"
    end
